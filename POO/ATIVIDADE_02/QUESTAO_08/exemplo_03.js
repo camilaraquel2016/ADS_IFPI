327b30e4f3aaaa71af07f743d434e878707ccd14
camilaraquel2016 <164662577+camilaraquel2016@users.noreply.github.com>
2025-09-09 18:57:31 -0300

    Create exemplo_03.js

---
+++ POO/ATIVIDADE_02/QUESTAO_08/exemplo_03.js
@@ -0,0 +1,28 @@
+console.log(0 == false); 
+console.log("" == false); 
+console.log(null == undefined); 
+
+/*
+saída:
+
+true
+true
+true
+
+No operador (==) o JavaScript faz coerção de tipo automática
+
+false --> 0
+" " --> 0
+
+null == undefined
+
+JS trata como null e undefined sendo iguais quando usando o operador ==
+
+
+OBS:
+
+O operador == não compara tipo e sim os valores em si.
+
+Para comparar tipos e valores ao mesmo tempo devemos usar === (igualdade estrita)
+Nesse caso de usar === todas as saídas seriam false, pois todas as comparações
+são com tipos diferentes, embora os valores quando convertidos sejam iguais.
