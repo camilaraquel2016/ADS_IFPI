e58d628c7906c9def6a4d227229c77c2fbbc7fb3
camilaraquel2016 <164662577+camilaraquel2016@users.noreply.github.com>
2025-09-09 18:44:53 -0300

    Create exemplo_02.js

---
+++ POO/ATIVIDADE_02/QUESTAO_08/exemplo_02.js
@@ -0,0 +1,15 @@
+let x = true;
+let y = 2;
+
+console.log(x + y);
+
+/*
+
+saída: 3
+
+Quando um boolean é usado em uma operação matemática, ele é convertido para número
+
+true -> 1
+false -> 0
+
+*/
