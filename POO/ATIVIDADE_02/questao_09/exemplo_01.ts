096dad8249778d165290b32c8dd9711811ab3436
camilaraquel2016 <164662577+camilaraquel2016@users.noreply.github.com>
2025-09-09 20:08:01 -0300

    Update exemplo_01.ts

--- POO/ATIVIDADE_02/questao_09/exemplo_01.ts
+++ POO/ATIVIDADE_02/questao_09/exemplo_01.ts
@@ -7,4 +7,7 @@ console.log(a + b);
 
 Parte do mesmo princípo que o JS, faz a concatenação, transformando number em string.
 
+Mesmo TypeScript sendo fortemente tipado, ele permite essa soma pois ele herda a regra
+do operador + do JS para string (se um dos operadores for uma string é feito então a concatenação)
+
 */ 
