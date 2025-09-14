4935a0723751dbb43cd37704d452e46d2175e86c
camilaraquel2016 <164662577+camilaraquel2016@users.noreply.github.com>
2025-09-14 14:03:04 -0300

    Create QUESTAO_18.ts

---
+++ POO/ATIVIDADE_02/QUESTAO_18.ts
@@ -0,0 +1,32 @@
+/*
+18. Alterne a opção strictNullChecks para true/false e descreva o que acontece com o
+exemplo abaixo:
+*/
+
+let nome: string;
+nome = "Ely";
+console.log("Nome:", nome);
+
+/*
+A configuração strictNullChecks quando ativada não permite que valores como 
+null ou undefined sejam atribuídos à variáveis normais (string, number, boolean).
+
+Ativar essa configuração ajuda a evitar bugs, garantindo que variáveis contenham sempre
+valores válidos do tipo esperado.
+
+Quando desativada, aumenta os riscos de erros em tempo de execução, porque null ou 
+undefined podem aparecer a qualquer momento.
+
+Há casos em que null ou undefined precisarão ser atribuídos à variáveis, por algum motivo.
+Nesse sentido, podemos usar tipos union, ou seja, mesmo que a configuração strictNullChecks
+esteja ativada, podemos declarar explicitamente que tal variável aceita além do seu tipo
+original, valores como null e undefined.
+
+Exemplo:
+*/
+
+let nome: string | null | undefined;
+
+/*
+Assim TypeScript sabe que null e undefined são permitidos nessa variável e a segurança
+ainda é mantida para os demais casos.
