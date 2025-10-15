683acd44f6889953ca3d1c6aa4a88837bc423480
Camila Raquel <raquelcamila909@gmail.com>
2025-10-15 16:33:12 -0300

    Adiciona arquivos

---
+++ ED/EXERCICIOS_CAP_04_FILAS/questao_03/main.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <ctype.h>
+#include "fila_char.h"
+#include "pilha_char.h"
+
+// palíndroma 4.3
+
+int eh_palindroma(char* cadeia) {
+	
+	Fila f = fila(256);
+	Pilha p = pilha(256);
+	
+	int i;
+	
+	for (i = 0; cadeia[i]; i++) {
+		
+		char c = cadeia[i];
+		
+		if (isalpha(c)) {
+			enfileira(toupper(c), f);
+			empilha(toupper(c), p);
+		}
+	}
+	
+	while (!vaziaf(f)) {
+		if (desenfileira(f) != desempilha(p)) {
+			destroif(&f);
+			destroip(&p);
+			return 0;
+		}
+	}
+	
+	destroif(&f);
+	destroip(&p);
+	return 1; 	
+}
+
+
+int main() {
+	
+	setlocale(LC_ALL, "Portuguese");
+	
+	char cadeia[256];
+	
+	printf("Insira a cadeia que deseja analisar: ");
+	fgets(cadeia, sizeof(cadeia), stdin);
+	
+	if (eh_palindroma(cadeia)) { 
+		printf("É palíndroma!\n");
+	}
+	else {
+		printf("Não é palíndroma!\n");
+	}
+	
+	return 0;
+}
---
+++ ED/EXERCICIOS_CAP_04_FILAS/questao_04/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include "fila_int.h"
+#define tempo 3
+
+int main() {
+	
+	setlocale(LC_ALL, "Portuguese");
+	
+	Fila f = fila(5);
+	
+	enfileira(17, f);  
+	enfileira(25, f);  
+	enfileira(39, f); 
+	enfileira(46, f); 
+	
+	while (!vaziaf(f)) {
+		int x = desenfileira(f);
+		int p = x / 10; 
+		int t = x % 10; 
+		
+		if (t > 3) enfileira(p*10+(t-tempo), f);
+		else printf("Processo %d concluído\n", p);
+	}
+	
+	destroif(&f);
+	
+	return 0;
+}
