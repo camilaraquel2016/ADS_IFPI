a6c4c6abe312dcf487f22c2d70759438abf273c7
Camila Raquel <raquelcamila909@gmail.com>
2025-11-29 15:16:40 -0300

    Adiciona arquivos da atividade 07 (herança)

--- POO/atividade_07_heranca/questao_09/src/folhaDePagamento.ts
+++ POO/atividade_07_heranca/questao_09/src/folhaDePagamento.ts
@@ -23,18 +23,3 @@ class FolhaDePagamento {
 }
 
 
-let p1 = new Pessoa('a', 'a');
-let p2 = new Funcionario('b', 'b', '123', 100);
-let p3 = new Professor('c', 'c', '123', 150, 'qq');
-
-let p4 = new Pessoa('d', 'd');
-let p5 = new Funcionario('e', 'e', '123', 15);
-let p6 = new Professor('f', 'f', '123', 130, 'qq');
-
-let pessoas: Array<Pessoa> = [];
-
-pessoas.push(p1, p2, p3, p4, p5, p6);
-
-let f1 = new FolhaDePagamento(pessoas);
-
-console.log(f1.calcularPagamentos());
